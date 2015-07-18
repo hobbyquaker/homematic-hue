df6135c3947201f44002fa15617a68b6793ba3ee
Markus B. <GitHub@markusbordihn.de>
2015-07-18 20:06:08 +0200

    Adding schedules.tcl

---
+++ hue/schedules.tcl
@@ -0,0 +1,18 @@
+#!/bin/tclsh
+
+source /usr/local/addons/hue/config.tcl
+
+if { $argc < 2 } {
+  puts "Usage: schedules.tcl schedule status"
+  puts ""
+  puts "  schedule              schedule id"
+  puts "  status                enabled/disabled"
+  exit 1
+}
+
+set schedule [lindex $argv 0]
+set status [lindex $argv 1]
+
+set url "http://$ip:80/api/$user/schedules/$schedule"
+
+exec echo "{\"status\":\"$status\"}" | /usr/local/addons/cuxd/curl -f -s -T - $u
